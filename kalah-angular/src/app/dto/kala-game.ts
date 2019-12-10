2d309e538a4d10c9044cdb67a3eeb910e897f716
Timothy Tseng <timothy.tseng@leaseplan.com>
2019-12-10 10:45:45 +0100

    backup

--- kalah-angular/src/app/dto/kala-game.ts
+++ kalah-angular/src/app/dto/kala-game.ts
@@ -1,9 +1,9 @@
-import {Game} from "./game";
+import {Game} from './game';
 
 export class KalaGame implements Game {
   id: string;
   title: string;
   player1: string;
   player2: string;
-
+  winner: string;
 }
